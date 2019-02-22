924b22ec9b351010422049da78be95a17bacd7a4
UKISTU18 <arajegan.aj@gmail.com>
2019-02-22 12:19:32 +0530

    aaawwwddd

--- app/public/js/script2.js
+++ app/public/js/script2.js
@@ -9,7 +9,7 @@ function addNote(){
   var params = "text="+text+"&title="+title;
 
   $.post(baseUrl+"notes",params,function(data){
-      document.getElementById("notes").innerHTML = "<tr><td id='id1'></td><td id='title1'></td><td id='body1'></td></tr>";
+      document.getElementById("notes").innerHTML = "<tr><td >"+data._id+"</td><td id='title1'></td><td id='body1'></td></tr>";
       document.getElementById("notes2").innerHTML ="<tr><td><button onclick='displayNote()'>Get Note</button></td><td><button onclick='editNote()'>Edit</button></td><td><button onclick='deleteNote()'>Delete</button></td></tr>"
       document.getElementById("add-title").value = "";
       document.getElementById("add-body").value = "";
